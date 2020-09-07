95d85d385d1c3c25736751e2e6e3f9cca0d0a784
KDSGYT <kdsgyt@gmail.com>
2020-09-07 09:27:49 -0400

    INput automatically changes to empty when enter is pressed

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,33 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import './input.scss';
 
-class Input extends Component  {
-    input = React.createRef();
+function Input({handleSubmit}) {
+    const input = React.useRef()
+    const [value, setValue] = React.useState()
+    return (
+        <form
+            className="input"
+            onSubmit={(e) => {
+                e.preventDefault();
+                handleSubmit(input.current.value)
+                input.current.value="";
+            }
+            }>
+            <input
+                value={value}
+                ref={input}
+                htmlFor="channel-search"
+                type="text"
+                className="label-input"
+                required
 
-    render(){
-        return (
-            <form 
-                className="input" 
-                onSubmit={ (e) => { e.preventDefault();
-                        this.props.handleSubmit(this.input.current.value)
-                    }
-                }>
-                <input
-                    ref={this.input}
-                    htmlFor="channel-search"
-                    type="text"
-                    className="label-input"
-                    required
-                />
-                <label htmlFor="channel-search" className="label-name">
-                    <span className="content-name">
-                        Search
+            />
+            <label htmlFor="channel-search" className="label-name">
+                <span className="content-name">
+                    Search
                     </span>
-                </label>
-                <input type="submit" className="material-icons" value="search" />
-            </form>
-        );
-    }
+            </label>
+            <input type="submit" className="material-icons" value="search" />
+        </form>
+    );
 }
 
 export default Input
